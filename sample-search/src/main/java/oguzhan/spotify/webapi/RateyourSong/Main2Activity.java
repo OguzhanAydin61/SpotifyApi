17e89258eddc10796027fbfef3c6f467cce21cba
OguzhanAydin61 <oguzhanaydin61@hotmail.com>
2017-07-27 11:23:11 +0300

    verison-6

--- sample-search/src/main/java/oguzhan/spotify/webapi/RateyourSong/Main2Activity.java
+++ sample-search/src/main/java/oguzhan/spotify/webapi/RateyourSong/Main2Activity.java
@@ -1,7 +1,13 @@
 package oguzhan.spotify.webapi.RateyourSong;
 
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.media.Image;
+import android.net.Uri;
+import android.os.IBinder;
+import android.support.v4.view.PagerAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,41 +17,123 @@ import android.widget.ListView;
 
 import com.spotify.sdk.android.authentication.AuthenticationClient;
 
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import oguzhan.spotify.webapi.android.SpotifyApi;
+import oguzhan.spotify.webapi.android.SpotifyCallback;
+import oguzhan.spotify.webapi.android.SpotifyError;
+import oguzhan.spotify.webapi.android.SpotifyService;
+import oguzhan.spotify.webapi.android.models.Pager;
+import oguzhan.spotify.webapi.android.models.Playlist;
+import oguzhan.spotify.webapi.android.models.PlaylistTrack;
+import oguzhan.spotify.webapi.android.models.Track;
+import oguzhan.spotify.webapi.android.models.UserPublic;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+import retrofit.http.Headers;
 
 public class Main2Activity extends AppCompatActivity {
+    public static final String TAG = "Main2Activity";
     private ArrayList<Tracks> tracks;
     private ListView listView;
+    private final List<Track> mItems = new ArrayList<>();
     private TracksResultsAdaptor listViewAdapter;
     private Button exit;
     private Button oyVer;
     private String usr;
     private String token;
+    private Image holdimage;
     static final String EXTRA_TOKEN = "EXTRA_TOKEN";
     private static final String KEY_CURRENT_QUERY = "CURRENT_QUERY";
+    private SpotifyService mService;
+    private PreviewPlayer mPlayer;
+    private String playlistURl;
+    private String trackname=" ";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         Intent intent = getIntent();
         token = intent.getStringExtra(EXTRA_TOKEN);
-        exit=(Button)findViewById(R.id.Exitbutton);
-        usr =intent.getStringExtra("isim");
-        Log.d("isimStart ",usr);
+        exit = (Button) findViewById(R.id.Exitbutton);
+        usr = intent.getStringExtra("isim");
+        Log.d("isimStart ", usr);
 
-        initialize();
-        fillArrayList(tracks);
+        SpotifyApi spotifyApi = new SpotifyApi();
+        spotifyApi.setAccessToken(token);
+        mService = spotifyApi.getService();
+        // Playlist weList=new Playlist();
+
+        playlistURl = "37i9dQZF1DXdnOj1VEuhgb";
+        mService.getUser(usr, new Callback<UserPublic>() {
+            @Override
+            public void success(UserPublic userPublic, Response response) {
+                Log.d(TAG, "user info is come: " + userPublic.display_name);
+
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                Log.e(TAG, "not come user info");
+            }
+        });
+      //  spotify:user:spotify:playlist:37i9dQZF1DXdnOj1VEuhgb
+        mService.getPlaylist("spotify", playlistURl, new SpotifyCallback<Playlist>() {
+            @Override
+            public void failure(SpotifyError error) {
+                Log.e(TAG, "Hata" + error.getMessage() + "  " + error.getLocalizedMessage());
+
+            }
+
+            @Override
+            public void success(Playlist playlist, Response response) {
+                //playlist.tracks.items.get(0).track.album.images.get(0);
+
+                for (int i = 0; i < playlist.tracks.items.size(); i++)
+                    Log.d("sarkilarimiz", "success: " + playlist.tracks.items.get(i).track.external_urls);
+
+                Map<Integer, Track> plyHold = new HashMap<>();
+                int size = playlist.tracks.items.size();
+                for (int i = 0; i < size; i++)
+                    plyHold.put(i, playlist.tracks.items.get(i).track);
+
+
+                // String muzik= playlist.tracks.items.get(0).track.preview_url.toString();
+                //mPlayer.play(muzik);
+                Log.e("item external: ", String.valueOf(playlist.tracks.items.get(0).track.external_urls.toString()));
+
+
+                initialize();
+                fillArrayList(tracks, plyHold);
+            }
+        });
+
+
+      /*
+        Pager<PlaylistTrack> playlistTracks=mService.getPlaylistTracks(usr,"spotify:user:spotify:playlist:37i9dQZF1DWXBcLLksEQAf");
+        List<PlaylistTrack> listemiz=new ArrayList<PlaylistTrack>();
+        listemiz.add(0,playlistTracks.items.get(0));
+       */
+
 
     }
-    public void onCikisButton(View view){
+
+    public void onCikisButton(View view) {
         AuthenticationClient.clearCookies(this);
-        Intent intent =new Intent(this,LoginActivity.class);
-        String tut=null;
-        intent.putExtra("Token",tut);
+        Intent intent = new Intent(this, LoginActivity.class);
+        String tut = null;
+        intent.putExtra("Token", tut);
         startActivity(intent);
         finish();
-
     }
+
     public static Intent createIntent(Context context) {
         return new Intent(context, Main2Activity.class);
     }
@@ -54,16 +142,29 @@ public class Main2Activity extends AppCompatActivity {
     private void initialize() {
         tracks = new ArrayList<Tracks>();
         listView = (ListView) findViewById(R.id.tracks_list_view);
-        listViewAdapter = new TracksResultsAdaptor(Main2Activity.this,tracks);
+        listViewAdapter = new TracksResultsAdaptor(Main2Activity.this, tracks);
         listView.setAdapter(listViewAdapter);
     }
 
-    private void fillArrayList(ArrayList<Tracks>tracks){
+    private void fillArrayList(ArrayList<Tracks> tracks, Map<Integer, Track> plyHold) {
 
-        for (int index = 0; index <10; index++) {//döngü şarkı size bağlı
-            Tracks sarki = new Tracks();//içini dolduracağız
+        for (int index = 0; index < plyHold.size(); index++) {//döngü şarkı size bağlı
+
+            Log.i("image: ", plyHold.get(index).album.images.get(0).url);
+            Uri b = Uri.parse(plyHold.get(index).album.images.get(0).url);
+            if(plyHold.get(index).preview_url!=null||plyHold.get(index).preview_url!=""){
+
+                trackname=plyHold.get(index).preview_url;
+            }else
+                trackname="Boş";
+
+
+
+            Tracks sarki = new Tracks(trackname, plyHold.get(index).name, plyHold.get(index).album.name,b );//içini dolduracağız
+
             tracks.add(sarki);
         }
+        listViewAdapter.notifyDataSetChanged();
 
-        }
+    }
 }
--- sample-search/src/main/java/oguzhan/spotify/webapi/RateyourSong/TracksResultsAdaptor.java
+++ sample-search/src/main/java/oguzhan/spotify/webapi/RateyourSong/TracksResultsAdaptor.java
@@ -1,7 +1,11 @@
 package oguzhan.spotify.webapi.RateyourSong;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,7 +13,16 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.media.Image;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.net.URL;
 import java.util.ArrayList;
 
 import oguzhan.spotify.webapi.android.models.Track;
@@ -19,22 +32,30 @@ import oguzhan.spotify.webapi.android.models.Track;
  */
 
 public class TracksResultsAdaptor extends ArrayAdapter<Tracks> {
+
     private final LayoutInflater inflater;
     private final Context context;
     private ViewHolder holder;
     private final ArrayList<Tracks> tracks;
-    public TracksResultsAdaptor(Context context, ArrayList<Tracks> tracks){
+    private int oylama;
+    private FirebaseDatabase db = FirebaseDatabase.getInstance();
+    private DatabaseReference yaz = db.getReference();
+    private int oy;
+
+    public TracksResultsAdaptor(Context context, ArrayList<Tracks> tracks) {
 
 
-        super(context,0, tracks);
+        super(context, 0, tracks);
         this.context = context;
         this.tracks = tracks;
         inflater = LayoutInflater.from(context);
     }
+
     @Override
-    public int getCount(){
+    public int getCount() {
         return tracks.size();
     }
+
     @Override
     public Tracks getItem(int position) {
         return tracks.get(position);
@@ -45,45 +66,78 @@ public class TracksResultsAdaptor extends ArrayAdapter<Tracks> {
         return tracks.get(position).hashCode();
     }
 
+
     @Override
-    public View getView(int position, View convertView, ViewGroup parent){
+    public View getView(int position, View convertView, ViewGroup parent) {
         if (convertView == null) {
 
             convertView = inflater.inflate(R.layout.oylama_list_view, null);
 
             holder = new ViewHolder();
-             holder.tracksName = (TextView) convertView.findViewById(R.id.baslik);
-            holder.tracksArtist = (TextView) convertView.findViewById(R.id.altbaslik);
-            holder.trackButon=(Button)convertView.findViewById(R.id.oylamaButton);
-            holder.imgview=(ImageView)convertView.findViewById(R.id.resimler);
+            holder.tracksName = (TextView) convertView.findViewById(R.id.SongName);
+            holder.tracksAlbum = (TextView) convertView.findViewById(R.id.AlbumName);
+            holder.tracksUrl = (TextView) convertView.findViewById(R.id.Urlname);
+            holder.trackButon = (Button) convertView.findViewById(R.id.oylamaButton);
+            holder.imgview = (ImageView) convertView.findViewById(R.id.resimler);
             convertView.setTag(holder);
 
-        }
-        else{
 
-            holder = (ViewHolder)convertView.getTag();
+        } else {
+
+            holder = (ViewHolder) convertView.getTag();
         }
 
         Tracks sarki = tracks.get(position);
-        if(sarki != null){
+        if (sarki != null) {
+
             holder.tracksName.setText(sarki.getTracksName());
-            holder.tracksArtist.setText(sarki.getTrackArtist());
-//holder.imgview.setImageURI(sarki.);
+            holder.tracksAlbum.setText(sarki.getTrackArtist());
+            holder.trackButon.setText("oylama");
+            holder.trackButon.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (v.getId() == R.id.oylamaButton) {
+                        DatabaseReference oku = FirebaseDatabase.getInstance().getReference().child("Tracks").child("Oylama");
+
+                        ValueEventListener dinle = new ValueEventListener() {
+                            @Override
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+
+
+                               oy=dataSnapshot.getValue(int.class);
+                                Log.i("Oylama Bilgisi", "Oy: " + oy);
+
+
+                            }
+
+                            @Override
+                            public void onCancelled(DatabaseError databaseError) {
+
+                            }
+
+                        };
+                         oku.addValueEventListener(dinle);
+                        oy++;
+                        yaz.child("Tracks").child("Oylama").setValue(oy);
+
+                    }
+                }
+            });
+            // holder.imgview.setImageURI(sarki.getImageUri());
+            Picasso.with(context).load(sarki.getImageUri()).into(holder.imgview);
+
+
         }
         return convertView;
     }
-    private static class ViewHolder implements View.OnClickListener{
+
+    private static class ViewHolder {
         TextView tracksName;
-        TextView tracksArtist;
+        TextView tracksAlbum;
+        TextView tracksUrl;
         Button trackButon;
         ImageView imgview;
 
-        @Override
-        public void onClick(View v) {
-            if(v.getId()==trackButon.getId()){
-
-            }
-        }
     }
 
 }
