2a3ad4f2fe6df135e8b0848663fd4ce5a3cef423
OguzhanAydin61 <oguzhanaydin61@hotmail.com>
2017-07-27 11:23:11 +0300

    version 5

--- sample-search/src/main/java/oguzhan/spotify/webapi/RateyourSong/Tracks.java
+++ sample-search/src/main/java/oguzhan/spotify/webapi/RateyourSong/Tracks.java
@@ -1,7 +1,16 @@
 package oguzhan.spotify.webapi.RateyourSong;
 
+import android.media.Image;
+import android.net.Uri;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.net.URL;
 
 /**
  * Created by oguzhanaydin on 24.07.2017.
@@ -12,34 +21,64 @@ public class Tracks {
     private String TracksUrl;
     private String TracksName;
     private String TrackArtist;
-    private String imageURL;
+    private Uri imageURL;
+    private int oylama;
     private FirebaseDatabase db = FirebaseDatabase.getInstance();
-    private DatabaseReference myRef = db.getReference();
+    //private DatabaseReference myRef = db.getReference();
+//    private static final String TAG = Tracks.class.getSimpleName();
 
-    public void setTracksUrl(String TracksUrl){
-        this.TracksUrl=TracksUrl;
+    public void setTracksUrl(String TracksUrl) {
+        this.TracksUrl = TracksUrl;
     }
-    public void setTracksName(String TracksName){
-        this.TracksName=TracksName;
+
+    public void setTracksName(String TracksName) {
+        this.TracksName = TracksName;
     }
-    public void setTrackArtist(String TrackArtist){
-        this.TrackArtist=TrackArtist;
+
+    public void setTrackArtist(String TrackArtist) {
+        this.TrackArtist = TrackArtist;
 
     }
-    public void setImageURL(String imageURL){
-        this.imageURL=imageURL;
+
+    public Tracks(String TracksUrl, String TracksName, String TrackArtist, Uri imageURL) {
+
+        this.TrackArtist = TrackArtist;
+        this.TracksUrl = TracksUrl;
+        this.TracksName = TracksName;
+        this.imageURL = imageURL;
+
     }
-    public String getTracksUrl(){
+
+    public void setOylama(int oylama) {
+        this.oylama = oylama;
+    }
+
+    public int getOylama() {
+
+        return oylama;
+    }
+
+    public void setImageUri(Uri imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    public String getTracksUrl() {
 
         return TracksUrl;
     }
-    public String getTracksName(){
+
+    public String getTracksName() {
 
         return TracksName;
     }
-    public String getTrackArtist(){
+
+    public String getTrackArtist() {
 
         return TrackArtist;
+    }
+
+    public Uri getImageUri() {
+        return imageURL;
     }
 
 
